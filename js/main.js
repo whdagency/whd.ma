98add68a80f535472d2f1960d9dd0828b3f26418
ayoub802 <71781766+ayoub802@users.noreply.github.com>
2023-07-10 13:25:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,6 +25,30 @@ gsap.utils.toArray(".reveal__banner").forEach((container) => {
   }, 0);
 });
 
+gsap.utils.toArray(".banner__bg").forEach((container) => {
+  let image = container.querySelector("img");
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      // start: "top 120%",
+      toggleActions: "restart none none reverse"
+    }
+  });
+
+  tl.set(container, { autoAlpha: 1 });
+  tl.from(container, {
+    duration: 3,
+    yPercent: 100,
+    skewX: 0.1,
+    ease: "expo"
+  });
+  tl.from(image, {
+    duration: 3,
+    yPercent: -100,
+    skewX: 0.1,
+    ease: "expo"
+  }, 0);
+});
 
 
 
